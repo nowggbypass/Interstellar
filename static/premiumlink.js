14777ed7169eecec7b6efa010bad4f6353fd44f8
nowggbypass <141445518+nowggbypass@users.noreply.github.com>
2023-11-25 00:18:54 -0600

    Create premiumlink.js

---
+++ static/premiumlink.js
@@ -0,0 +1,17 @@
+ var password = window.localStorage.getItem('password');
+        var validPassword = "1234";
+        if (password && password === validPassword) {
+            document.body.style.display = 'block';
+        } else {
+            var input = prompt('This Premium Link Is Password Protected. Enter The Password:');
+            if (input === validPassword) {
+                window.localStorage.setItem('password', input);
+                document.body.style.display = 'block';
+            } else {
+                alert('Invalid password you little nigga.');
+                window.location.href = 'index.html';
+            }
+        }
+        if (document.body.style.display !== 'block') {
+            document.body.style.display = 'none';
+        } 
